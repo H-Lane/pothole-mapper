6f014a5ee100eead4165b2fed9711f5f690481f8
chezketchum <williamschez89@gmail.com>
2024-06-06 20:29:18 -0400

    added models and database

---
+++ models/Comments.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    // Manually define the primary key
+    Comments_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    author: {
+      type: DataTypes.STRING
+    },
+    isbn: {
+      type: DataTypes.STRING
+    },
+    pages: {
+      type: DataTypes.INTEGER
+    },
+    edition: {
+      type: DataTypes.INTEGER
+    },
+    is_paperback: {
+      type: DataTypes.BOOLEAN
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    // Prevent sequelize from renaming the table
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments'
+  }
+);
+
+module.exports=Comments;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Pothole=require(`./Pothole`);
+const Users=require(`./Users`);
+const Comments=require(`./Comments`);
+
+
+module.exports={ Pothole, Users, Comments }
