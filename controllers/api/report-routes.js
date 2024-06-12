93c3ed78e1a984672d2ee370d14ebaf38f07251f
Hersey <helane1996@gmail.com>
2024-06-12 19:59:18 -0400

    finished comment routes and added report routes

--- controllers/api/report-routes.js
+++ controllers/api/report-routes.js
@@ -1,5 +1,19 @@
 const router = require(`express`).Router();
-const User = require(`../../models`);
+const { User, Pothole, Comments } = require(`../../models`);
 
+router.post(`/`, async (req, res) => {
+  try {
+    const dbPotholeData = await Pothole.findAll({
+        where: {user_id: req.body.user_id}
+    });
+    const dbCommentsData = await Comments.findAll({
+        where: {user_id: req.body.user_id}
+    });
+    res.status(200).json(dbPotholeData, dbCommentsData)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
