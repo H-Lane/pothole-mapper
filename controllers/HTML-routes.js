a59e45144baa61cc97a49485f8a429d877c84682
Hersey <helane1996@gmail.com>
2024-06-12 22:04:08 -0400

    added some additional code for the reports.handlebars page and fixed some withAuth requirements for my routes

--- controllers/HTML-routes.js
+++ controllers/HTML-routes.js
@@ -1,12 +1,14 @@
 //require in our Express router
 const router = require("express").Router();
 const { User, Pothole, Comments } = require(`../models`);
-//route for the homepage
+const { withAuth } = require(`../utils/auth`);
+
+//route for the homepage and pass it all potholes and all comments in our db
 router.get(`/`, async (req, res) => {
   try {
     const potholes = await Pothole.findAll();
     const comments = await Comments.findAll();
-    res.render(`HOMEPAGEPLACEHOLDER`, { potholes, comments });
+    res.render(`HOMEPAGEPLACEHOLDER`, { potholes, comments, logged_in: req.session.logged_in });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -14,12 +16,17 @@ router.get(`/`, async (req, res) => {
 });
 
 // the home route for the login page
-router.get(`/login`, async (req, res) => {
+router.get(`/login`, (req, res) => {
   res.render(`LOGINPAGEPLACEHOLDER`);
 });
 
-//grab the account details html page
-router.get(`/account`, async (req, res) => {
+//grab the account details html page and pass it the users potholes and comments
+router.get(`/account`, withAuth, async (req, res) => {
+    res.render(`ACCOUNTHTMLPLACEHOLDER`, { logged_in: req.session.logged_in });
+});
+
+//Get HTML route for the Users reported potholes
+router.get(`/reports`, withAuth, async (req, res) => {
   try {
     const userPotholes = await Pothole.findAll({
       where: { user_id: req.session.user_id },
@@ -27,16 +34,11 @@ router.get(`/account`, async (req, res) => {
     const userComments = await Comments.findAll({
       where: { user_id: req.session.user_id },
     });
-    res.render(`ACCOUNTHTMLPLACEHOLDER`, { userPotholes, userComments });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-//Get HTML route for the Users reported potholes
-router.get(`/reports`, (req, res) => {
-  res.render(`REPORTSHTMLPLACEHOLDER`);
+  res.render(`REPORTSHTMLPLACEHOLDER`, { userPotholes, userComments, logged_in: req.session.logged_in });
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
