fca381d830145ce3b5a84457aacbdf9e29ddfd18
Hersey <helane1996@gmail.com>
2024-06-12 21:15:53 -0400

    passed potholes and comments to handlebars methods for displaying reports and homepage

--- controllers/HTML-routes.js
+++ controllers/HTML-routes.js
@@ -1,9 +1,16 @@
 //require in our Express router
 const router = require("express").Router();
-
+const { User, Pothole, Comments } = require(`../models`);
 //route for the homepage
 router.get(`/`, async (req, res) => {
-  res.render(`HOMEPAGEPLACEHOLDER`);
+  try {
+    const potholes = await Pothole.findAll();
+    const comments = await Comments.findAll();
+    res.render(`HOMEPAGEPLACEHOLDER`, { potholes, comments });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // the home route for the login page
@@ -12,8 +19,19 @@ router.get(`/login`, async (req, res) => {
 });
 
 //grab the account details html page
-router.get(`/account`, (req, res) => {
-  res.render(`ACCOUNTHTMLPLACEHOLDER`);
+router.get(`/account`, async (req, res) => {
+  try {
+    const userPotholes = await Pothole.findAll({
+      where: { user_id: req.session.user_id },
+    });
+    const userComments = await Comments.findAll({
+      where: { user_id: req.session.user_id },
+    });
+    res.render(`ACCOUNTHTMLPLACEHOLDER`, { userPotholes, userComments });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 //Get HTML route for the Users reported potholes
