cb7bd211ba1cbf948801590d47c1bc7cb02ffdb0
Hersey <helane1996@gmail.com>
2024-06-16 01:50:49 -0400

    fixed routes for reports and potholes to return correct data

--- controllers/api/report-routes.js
+++ controllers/api/report-routes.js
@@ -3,13 +3,17 @@ const { User, Pothole, Comments } = require(`../../models`);
 
 router.get(`/`, async (req, res) => {
   try {
-    const potholes = await Pothole.findAll({
+    const userPotholes = await Pothole.findAll({
         where: {user_id: req.session.user_id}
     });
-    const comments = await Comments.findAll({
+    const userComments = await Comments.findAll({
         where: {user_id: req.session.user_id}
     });
-    res.status(200).json({ potholes, comments })
+
+    const potholes = userPotholes.toJSON();
+    const comments = userComments.toJSON();
+
+    res.status(200).json({ potholes: potholes, comments: comments })
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
